4f6e8524015c31a0a9b2f9df559ce61c308a3b4b
Vladimir <nekrasowladimir@gmail.com>
2016-12-18 14:19:43 +0900

    Add files via upload

---
+++ evaluate.py
@@ -0,0 +1,127 @@
+"""Evaluation script for the DeepLab-LargeFOV network on the validation subset
+   of PASCAL VOC dataset.
+
+This script evaluates the model on around 1500 validation images.
+"""
+
+from __future__ import print_function
+
+import argparse
+from datetime import datetime
+import os
+import sys
+import time
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import tensorflow as tf
+import numpy as np
+
+from deeplab_lfov import DeepLabLFOVModel, ImageReader, decode_labels
+
+DATA_DIRECTORY = '/home//VOCdevkit'
+DATA_LIST_PATH = './dataset/val.txt'
+NUM_STEPS = 1449
+RESTORE_FROM = None
+SAVE_DIR = './images_val/'
+WEIGHTS_PATH   = './util/net_weights.ckpt'
+
+IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
+
+def get_arguments():
+    """Parse all the arguments provided from the CLI.
+    
+    Returns:
+      A list of parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="DeepLabLFOV Network")
+    parser.add_argument("--data_dir", type=str, default=DATA_DIRECTORY,
+                        help="Path to the directory containing the PASCAL VOC dataset.")
+    parser.add_argument("--data_list", type=str, default=DATA_LIST_PATH,
+                        help="Path to the file listing the images in the dataset.")
+    parser.add_argument("--num_steps", type=int, default=NUM_STEPS,
+                        help="Number of images in the validation set.")
+    parser.add_argument("--restore_from", type=str, default=RESTORE_FROM,
+                        help="Where restore model parameters from.")
+    parser.add_argument("--save_dir", type=str, default=SAVE_DIR,
+                        help="Where to save predicted masks.")
+    parser.add_argument("--weights_path", type=str, default=WEIGHTS_PATH,
+                        help="Path to the file with caffemodel weights. "
+                             "If not set, all the variables are initialised randomly.")
+    return parser.parse_args()
+
+def load(saver, sess, ckpt_path):
+    '''Load trained weights.
+    
+    Args:
+      saver: TensorFlow saver object.
+      sess: TensorFlow session.
+      ckpt_path: path to checkpoint file with parameters.
+    ''' 
+    saver.restore(sess, ckpt_path)
+    print("Restored model parameters from {}".format(ckpt_path))
+
+def main():
+    """Create the model and start the evaluation process."""
+    args = get_arguments()
+    
+    # Create queue coordinator.
+    coord = tf.train.Coordinator()
+    
+    # Load reader.
+    with tf.name_scope("create_inputs"):
+        reader = ImageReader(
+            args.data_dir,
+            args.data_list,
+            input_size=None,
+            random_scale=False,
+            coord=coord)
+        image, label = reader.image, reader.label
+    image_batch, label_batch = tf.expand_dims(image, dim=0), tf.expand_dims(label, dim=0) # Add the batch dimension.
+    # Create network.
+    net = DeepLabLFOVModel(args.weights_path)
+
+    # Which variables to load.
+    trainable = tf.trainable_variables()
+    
+    # Predictions.
+    pred = net.preds(image_batch)
+    
+    # mIoU
+    mIoU, update_op = tf.contrib.metrics.streaming_mean_iou(pred, label_batch, num_classes=21) 
+    
+    # Set up tf session and initialize variables. 
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    init = tf.initialize_all_variables()
+    
+    sess.run(init)
+    sess.run(tf.initialize_local_variables())
+    
+    # Load weights.
+    saver = tf.train.Saver(var_list=trainable)
+    if args.restore_from is not None:
+        load(saver, sess, args.restore_from)
+    
+    # Start queue threads.
+    threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+    
+    # Iterate over images.
+    for step in range(args.num_steps):
+        #mIoU_value = sess.run([mIoU])
+        #_ = update_op.eval(session=sess)
+        preds, _ = sess.run([pred, update_op])
+        
+        if args.save_dir is not None:
+            img = decode_labels(preds[0, :, :, 0])
+            im = Image.fromarray(img)
+            im.save(args.save_dir + str(step) + '.png')
+        if step % 100 == 0:
+            print('step {:d} \t'.format(step))
+    print('Mean IoU: {:.3f}'.format(mIoU.eval(session=sess)))
+    coord.request_stop()
+    coord.join(threads)
+    
+if __name__ == '__main__':
+    main()
---
+++ train.py
@@ -0,0 +1,151 @@
+"""Training script for the DeepLab-LargeFOV network on the PASCAL VOC dataset
+   for semantic image segmentation.
+
+This script trains the model using augmented PASCAL VOC dataset,
+which contains approximately 10000 images for training and 1500 images for validation.
+"""
+
+from __future__ import print_function
+
+import argparse
+from datetime import datetime
+import os
+import sys
+import time
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+import numpy as np
+
+from deeplab_lfov import DeepLabLFOVModel, ImageReader, decode_labels
+
+BATCH_SIZE = 16
+DATA_DIRECTORY = '/home/VOCdevkit'
+DATA_LIST_PATH = './dataset/train.txt'
+INPUT_SIZE = '321,321'
+LEARNING_RATE = 1e-4
+MEAN_IMG = tf.Variable(np.array((104.00698793,116.66876762,122.67891434)), trainable=False, dtype=tf.float32)
+NUM_STEPS = 20000
+RANDOM_SCALE = True
+SAVE_DIR = './images/'
+SAVE_NUM_IMAGES = 2
+SAVE_PRED_EVERY = 500
+SNAPSHOT_DIR = './snapshots/'
+WEIGHTS_PATH   = "./util/weights.ckpt"
+
+IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
+
+def get_arguments():
+    """Parse all the arguments provided from the CLI.
+    
+    Returns:
+      A list of parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="DeepLabLFOV Network")
+    parser.add_argument("--batch_size", type=int, default=BATCH_SIZE,
+                        help="Number of images sent to the network in one step.")
+    parser.add_argument("--data_dir", type=str, default=DATA_DIRECTORY,
+                        help="Path to the directory containing the PASCAL VOC dataset.")
+    parser.add_argument("--data_list", type=str, default=DATA_LIST_PATH,
+                        help="Path to the file listing the images in the dataset.")
+    parser.add_argument("--input_size", type=str, default=INPUT_SIZE,
+                        help="Comma-separated string with height and width of images.")
+    parser.add_argument("--learning_rate", type=float, default=LEARNING_RATE,
+                        help="Learning rate for training.")
+    parser.add_argument("--num_steps", type=int, default=NUM_STEPS,
+                        help="Number of training steps.")
+    parser.add_argument("--save_dir", type=str, default=SAVE_DIR,
+                        help="Where to save figures with predictions.")
+    parser.add_argument("--save_num_images", type=int, default=SAVE_NUM_IMAGES,
+                        help="How many images to save.")
+    parser.add_argument("--save_pred_every", type=int, default=SAVE_PRED_EVERY,
+                        help="Save figure with predictions and ground truth every often.")
+    parser.add_argument("--snapshot_dir", type=str, default=SNAPSHOT_DIR,
+                        help="Where to save snapshots of the model.")
+    parser.add_argument("--weights_path", type=str, default=WEIGHTS_PATH,
+                        help="Path to the file with caffemodel weights. "
+                             "If not set, all the variables are initialised randomly.")
+    return parser.parse_args()
+
+def save(saver, sess, logdir, step):
+    model_name = 'model.ckpt'
+    checkpoint_path = os.path.join(logdir, model_name)
+
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+
+    saver.save(sess, checkpoint_path, global_step=step)
+    print('The checkpoint has been created.')
+
+def main():
+    """Create the model and start the training."""
+    args = get_arguments()
+    
+    h, w = map(int, args.input_size.split(','))
+    input_size = (h, w)
+    
+    # Create queue coordinator.
+    coord = tf.train.Coordinator()
+    
+    # Load reader.
+    with tf.name_scope("create_inputs"):
+        reader = ImageReader(
+            args.data_dir,
+            args.data_list,
+            input_size,
+            RANDOM_SCALE,
+            coord)
+        image_batch, label_batch = reader.dequeue(args.batch_size)
+    
+    # Create network.
+    net = DeepLabLFOVModel(args.weights_path)
+
+    # Define the loss and optimisation parameters.
+    loss = net.loss(image_batch, label_batch)
+    optimiser = tf.train.AdamOptimizer(learning_rate=args.learning_rate)
+    trainable = tf.trainable_variables()
+    optim = optimiser.minimize(loss, var_list=trainable)
+    
+    pred = net.preds(image_batch)
+    
+    # Set up tf session and initialize variables. 
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    init = tf.initialize_all_variables()
+    
+    sess.run(init)
+    
+    # Saver for storing checkpoints of the model.
+    saver = tf.train.Saver(var_list=trainable, max_to_keep=40)
+    
+    # Start queue threads.
+    threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+    
+    # Iterate over training steps.
+    for step in range(args.num_steps):
+        start_time = time.time()
+        
+        if step % args.save_pred_every == 0:
+            loss_value, images, labels, preds, _ = sess.run([loss, image_batch, label_batch, pred, optim])
+            fig, axes = plt.subplots(args.save_num_images, 3, figsize = (16, 12))
+            for i in xrange(args.save_num_images):
+                axes.flat[i * 3].set_title('data')
+                axes.flat[i * 3].imshow((images[i] + IMG_MEAN)[:, :, ::-1].astype(np.uint8))
+
+                axes.flat[i * 3 + 1].set_title('mask')
+                axes.flat[i * 3 + 1].imshow(decode_labels(labels[i, :, :, 0]))
+
+                axes.flat[i * 3 + 2].set_title('pred')
+                axes.flat[i * 3 + 2].imshow(decode_labels(preds[i, :, :, 0]))
+            plt.savefig(args.save_dir + str(start_time) + ".png")
+            save(saver, sess, args.snapshot_dir, step)
+        else:
+            loss_value, _ = sess.run([loss, optim])
+        duration = time.time() - start_time
+        print('step {:d} \t loss = {:.3f}, ({:.3f} sec/step)'.format(step, loss_value, duration))
+    coord.request_stop()
+    coord.join(threads)
+    
+if __name__ == '__main__':
+    main()
